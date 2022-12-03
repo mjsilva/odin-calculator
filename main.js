937df18eebb908dd59a72323f45f83da1e867d76
Manuel Silva <manuel@mjs.me>
2022-12-03 17:19:24 +0000

    Adds symbol logic to visor update
    Added symbol logic to visor. Also switched the visor screen so it shows the whole operation on the main display. Thinking on having the small display show the result of the whole operation.
    
    We're half way there.

--- main.js
+++ main.js
@@ -1,15 +1,70 @@
+let operationsStack = [];
+const MATHS_OPERATIONS = ["+", "-", "×", "÷"];
+
 const allClear = () => {
-  document.querySelector(".results").innerHTML = "";
   document.querySelector(".operation").innerHTML = "";
+  document.querySelector(".result").innerHTML = "";
+  operationsStack = [];
+};
+
+const calculateAndUpdateResult = () => {};
+
+const processCalculatorLogic = (userOperationValue) => {
+  // no more than one operation symbol
+  // if an operation symbol already exists replace the last one
+  const lastValue = operationsStack.slice(-1)[0];
+  const currentValueIsAnOperation =
+    MATHS_OPERATIONS.includes(userOperationValue);
+  const lastValueWasAnOperation = MATHS_OPERATIONS.includes(lastValue);
+  let valueToInsert = userOperationValue;
+
+  // REPLACE SYMBOL IF PREVIOUS EXISTS
+  // remove last value and replace it with the new one if
+  // last value is an operation symbol
+  // example: if a user previously clicked on "+" and then on "*"
+  // "+" should be replaced by "*"
+  if (lastValueWasAnOperation && currentValueIsAnOperation) {
+    operationsStack.pop();
+    valueToInsert = userOperationValue;
+    updateOperationsStackAndVisor(valueToInsert);
+    return;
+  }
+
+  // APPEND NUMBER TO STACK
+  // if value is a number and last value was a number too instead of adding
+  // a new index on the array just append the new number to old number
+  if (!lastValueWasAnOperation && !currentValueIsAnOperation) {
+    valueToInsert = operationsStack.length
+      ? operationsStack.pop() + userOperationValue
+      : userOperationValue;
+    updateOperationsStackAndVisor(valueToInsert);
+    return;
+  }
+
+  updateOperationsStackAndVisor(valueToInsert);
+};
+
+const updateOperationsStackAndVisor = (value) => {
+  operationsStack.push(value);
+  updateVisorResultsElementBaseOnOperationStack();
+  console.log("operationsStack", operationsStack);
+};
+
+const updateVisorResultsElementBaseOnOperationStack = () => {
+  const el = document.querySelector(".operation");
+  el.innerHTML = operationsStack.join("");
 };
 
 const clearLastDigit = () => {
-  const el = document.querySelector(".results");
+  const el = document.querySelector(".operation");
   el.innerHTML = el.innerHTML.substring(0, el.innerHTML.length - 1);
+  operationsStack.pop();
 };
 const handleNumberKeys = (number) => {
-  const el = document.querySelector(".results");
-  el.innerHTML += number;
+  processCalculatorLogic(number);
+};
+const handleMathsOperation = (operationSymbol) => {
+  processCalculatorLogic(operationSymbol);
 };
 // even binding
 const handleKeyClick = (e) => {
@@ -27,8 +82,11 @@ const handleKeyClick = (e) => {
     case operation === false: // it's a number
       handleNumberKeys(keyValue);
       break;
+    case MATHS_OPERATIONS.includes(keyValue): // it's mathematical operation
+      handleMathsOperation(keyValue);
+      break;
   }
-  console.log(e.currentTarget.innerHTML);
+  console.log("e.currentTarget.innerHTML", e.currentTarget.innerHTML);
 };
 [...document.querySelectorAll(".key")].forEach((el) => {
   el.addEventListener("click", handleKeyClick);
